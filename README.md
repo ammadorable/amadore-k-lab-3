f148acb97bf5696160a050f8cd0776e678e4278e
ammadorable <akozeliski29@setoncatholic.org>
2026-09-23 20:15:34 +0000

    forst ccommit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# amadore-k-lab-3
+#this function adds two numbers
+def add(x,y)
+    print(x+y)
+
+add(6,7)
+
+#this function adds two numbers
+def sub(x,y)
+    print(x-y)
+
+add(6,7)
+
+#this function adds two numbers
+def mul(x,y)
+    print(x*y)
+
+add(6,7)
+
+#this function adds two numbers
+def div(x,y)
+    print(x/y)
+
+add(6,7)
+
+add(6,7)
+sub(6,7)
+div(6,7)
+mul(6,7)
---
+++ main.py
@@ -0,0 +1,23 @@
+# this function adds two numbers
+def add(x, y):
+    return x + y
+
+# this function subtracts two numbers
+def sub(x, y):
+    return x - y
+
+# this function multiplies two numbers
+def mul(x, y):
+    return x * y
+
+# this function divides two numbers
+def div(x, y):
+    return x / y
+
+x = int(input("enter your first number: "))
+y = int(input("enter your second number: "))
+
+print("add:", add(x, y))
+print("subtract:", sub(x, y))
+print("multiply:", mul(x, y))
+print("divide:", div(x, y))
